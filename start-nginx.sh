6e24f39918472799591941af682d7a2a83886e98
hartraft <hartraft@tutanota.com>
2022-12-21 11:44:51 +0100

    Fix truncated placeholder replace
    From @dcdh https://github.com/nodeshift-blog-examples/frontend-containers/issues/3

--- start-nginx.sh
+++ start-nginx.sh
@@ -1,7 +1,11 @@
-#!/usr/bin/env bash
-export EXISTING_VARS=$(printenv | awk -F= '{print $1}' | sed 's/^/\$/g' | paste -sd,); 
+#!/usr/bin/env sh
+export EXISTING_VARS=$(printenv | awk -F= '{print $1}' | sed 's/^/\$/g' | paste -sd,);
 for file in $JSFOLDER;
 do
-  cat $file | envsubst $EXISTING_VARS | tee $file
+  if [ ! -f $file.tmp ]
+  then
+    cp $file $file.tmp
+  fi
+  cat $file.tmp | envsubst $EXISTING_VARS | tee $file
 done
 nginx -g 'daemon off;'
